031eaedea5185134804a9574c721675e63a2da21
alledanse <andrew.alledanse@gmail.com>
2025-10-07 16:41:03 +0300

    init project

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,110 @@
+import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
+
+test.beforeEach(async ({ page }) => {
+  await page.route('*/**/api/tags', async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
+  await page.goto('https://conduit.bondaracademy.com/');
+  await page.getByText('Sign in').click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('pw123test@test.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Welcome123');
+  await page.getByRole('button').click();
+});
+
+test('has title', async ({ page }) => {
+  await page.route('*/**/api/articles*', async (route) => {
+    const response = await route.fetch();
+    const data = await response.json();
+    data.articles[0].title = 'This is a mock test title';
+    data.articles[0].description = 'This is a mock description';
+
+    await route.fulfill({
+      body: JSON.stringify(data),
+    });
+  });
+  await page.getByText('Global Feed').click();
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  await expect(page.locator('app-article-list h1').first()).toContainText(
+    'This is a mock test title',
+  );
+  await expect(page.locator('app-article-list p').first()).toContainText(
+    'This is a mock  description',
+  );
+});
+
+test('delete article', async ({ page, request }) => {
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      user: { email: 'pw123test@test.com', password: 'Welcome123' },
+    },
+  });
+  const data = await response.json();
+  const accessToken = data.user.token;
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles', {
+    data: {
+      article: {
+        tagList: [],
+        title: 'This is a test title',
+        description: 'This is a test description',
+        body: 'This is a test body',
+      },
+    },
+    headers: {
+      Authorization: `Token ${accessToken}`,
+    },
+  });
+  expect(articleResponse.status()).toEqual(201);
+  await page.getByText('Global Feed').click();
+  await page.getByText('This is a test title').click();
+  await page.getByRole('button', { name: 'Delete Article' }).first().click();
+  await page.getByText('Global Feed').click();
+
+  await expect(page.locator('app-article-list h1').first()).not.toContainText(
+    'This is a mock test title',
+  );
+});
+
+test('create article', async ({ page, request }) => {
+  await page.getByText('New Article').click();
+  await page.getByRole('textbox', { name: 'Article Title' }).fill('Playwright is awesome');
+  await page
+    .getByRole('textbox', { name: "What's this article about?" })
+    .fill('About the Playwright');
+  await page
+    .getByRole('textbox', { name: 'Write your article (in markdown)' })
+    .fill('We like use Playwright for automation');
+  await page.getByRole('button', { name: 'Publish Article' }).click();
+  const articleResponse = await page.waitForResponse(
+    'https://conduit-api.bondaracademy.com/api/articles/',
+  );
+  const articleResponseBody = await articleResponse.json();
+  const slugId = articleResponseBody.article.slug;
+
+  await expect(page.locator('.article-page h1')).toContainText('Playwright is awesome');
+  await page.getByText('Home').click();
+  await page.getByText('Global Feed').click();
+  await expect(page.locator('app-article-list h1').first()).toContainText('Playwright is awesome');
+
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {
+      user: { email: 'pw123test@test.com', password: 'Welcome123' },
+    },
+  });
+  const data = await response.json();
+  const accessToken = data.user.token;
+
+  const deleteArticleResponse = await request.delete(
+    `https://conduit-api.bondaracademy.com/api/articles/${slugId}`,
+    {
+      headers: {
+        Authorization: `Token ${accessToken}`,
+      },
+    },
+  );
+  expect(deleteArticleResponse.status()).toEqual(204);
+});
+
+//https://conduit-api.bondaracademy.com
